447e6687fdca5dcdfe6ee3db258c89273317b205
Paulina <paulina.olszewska@intel.com>
2023-01-09 14:32:53 +0100

    update versions, typos, kafka cluster on node labeled retail-infra

--- scenario-player/.environment.variables.sh
+++ scenario-player/.environment.variables.sh
@@ -3,6 +3,6 @@ export STORE_WIDTH=6
 export CUSTOMERS_AVERAGE_IN_STORE=6
 export CUSTOMERS_LIST_FILE='customers.csv'
 export MQTT_HOST='127.0.0.1'
-export MQTT_NAME=test1
+export MQTT_NAME=ex-aao-mqtt-0-svc
 
 export LOG_LEVEL=INFO
